17f2147d4973d4a0285ea3427496944808142aa3
Nicholas <nicholas.roether@t-online.de>
2022-10-11 23:11:03 +0200

    Add base css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import { useEffect } from "react";
 import { ThemeProvider } from "@emotion/react";
 import Page from "./components/Page";
 import theme from "./theme";
 
 function App(): JSX.Element {
-	useEffect(() => {
-		const rootElement = document.querySelector(":root");
-		if (!rootElement || !(rootElement instanceof HTMLHtmlElement))
-			throw new Error("Could not find root element!");
-		rootElement.style.backgroundColor = theme.colors.background[500];
-	}, []);
-
 	return (
 		<ThemeProvider theme={theme}>
 			<Page></Page>
