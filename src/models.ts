beb4b2b8208df02306001721ec9941c1359690a2
Nicholas <nicholas.roether@t-online.de>
2022-10-19 20:05:43 +0200

    Re-add user schema in models

--- src/models.ts
+++ src/models.ts
@@ -1,3 +1,5 @@
+import schema, { Validator } from "./utils/schema";
+
 interface User {
 	id: string;
 	name: string;
@@ -12,5 +14,22 @@ interface User {
 		awarder: number;
 	};
 }
+
+const userValidator: Validator<User> = schema.object({
+	id: schema.string(),
+	name: schema.string(),
+	displayName: schema.string(),
+	profilePicture: schema.string(),
+	created: schema.number().integer(),
+	karma: schema.object({
+		total: schema.number().integer(),
+		post: schema.number().integer(),
+		comment: schema.number().integer(),
+		awardee: schema.number().integer(),
+		awarder: schema.number().integer()
+	})
+});
+
+export { userValidator };
 
 export type { User };
