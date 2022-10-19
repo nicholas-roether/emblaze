958ea710eb8e7df8c8e10ec21581611cb948aec5
Nicholas <nicholas.roether@t-online.de>
2022-10-19 20:06:42 +0200

    Add temporary debug user data display

--- src/components/page/UserSection.tsx
+++ src/components/page/UserSection.tsx
@@ -1,12 +1,20 @@
 import { FiUser } from "react-icons/fi";
+import dataHooks from "../../data_hooks";
 import Button from "../lib/button";
 import Icon from "../lib/Icon";
 
 function UserSection(): JSX.Element {
+	const { data: user, error } = dataHooks.useMe();
+
+	if (error) throw error;
+
 	return (
-		<Button href="/api/auth/login">
-			Log in <Icon css={{ verticalAlign: "bottom" }}>{FiUser}</Icon>
-		</Button>
+		<>
+			{JSON.stringify(user)}
+			<Button href="/api/auth/login">
+				Log in <Icon css={{ verticalAlign: "bottom" }}>{FiUser}</Icon>
+			</Button>
+		</>
 	);
 }
 
