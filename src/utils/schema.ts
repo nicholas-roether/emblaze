846cb629ef7235da2c33008f5add81d7e0d21d9e
Nicholas Roether <nicholas.roether@t-online.de>
2022-10-19 15:24:40 +0200

    Add error messages to schema

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,20 +1,47 @@
-interface Validator<T> {
-	readonly type: string | null;
-	check(val: unknown): val is T;
-}
+class AssertionError extends Error {}
 
-abstract class SchemaCondition<C, R extends C> implements Validator<R> {
-	private readonly base: Validator<C>;
+abstract class Validator<T> {
+	name: string;
 	readonly type: string | null;
 
-	constructor(base: Validator<C>, type: string | null = null) {
-		this.base = base;
-		this.type = type ?? base.type;
+	constructor(name = "schema", type: string | null) {
+		this.name = name;
+		this.type = type;
 	}
 
-	check(val: unknown): val is R {
-		if (!this.base.check(val)) return false;
-		return this.condition(val);
+	abstract assert(val: unknown): asserts val is T;
+
+	check(val: unknown): val is T {
+		try {
+			this.assert(val);
+			return true;
+		} catch (err) {
+			if (err instanceof AssertionError) return false;
+			throw err;
+		}
+	}
+}
+
+abstract class SchemaCondition<C, R extends C> extends Validator<R> {
+	private readonly base: Validator<C>;
+	readonly message: string;
+
+	constructor(
+		base: Validator<C>,
+		message?: string,
+		type: string | null = null,
+		name = "schema"
+	) {
+		super(name ?? base.name, type ?? base.type);
+		this.base = base;
+		this.message = message ?? `Expected ${type}`;
+	}
+
+	assert(val: unknown): asserts val is R {
+		this.base.assert(val);
+		if (!this.condition(val)) {
+			throw new AssertionError(this.message);
+		}
 	}
 
 	protected abstract condition(val: C): val is R;
@@ -40,7 +67,10 @@ class OptionCondition<C, O extends C> extends SchemaCondition<C, C & O> {
 	private readonly options: O[];
 
 	constructor(base: Validator<C>, options: O[]) {
-		super(base);
+		super(
+			base,
+			`Expected one of: ${options.map((o) => JSON.stringify(o)).join(", ")}`
+		);
 		this.options = options;
 	}
 
@@ -56,7 +86,7 @@ class ExactCondition<C, T extends C> extends SchemaCondition<C, T> {
 	private readonly value: T;
 
 	constructor(base: Validator<C>, value: T) {
-		super(base);
+		super(base, `Expected: ${JSON.stringify(value)}`);
 		this.value = value;
 	}
 
@@ -66,38 +96,53 @@ class ExactCondition<C, T extends C> extends SchemaCondition<C, T> {
 }
 
 class IntegerCondition<C extends number> extends SchemaCondition<C, C> {
+	constructor(base: Validator<C>) {
+		super(base, `Expected integer`);
+	}
+
 	protected condition(val: C): val is C {
 		return Number.isInteger(val);
 	}
 }
 
-class TypeSchema<T> extends SchemaCondition<T, T> {
-	constructor(type: string | null) {
-		super({ type, check: (val): val is T => !type || typeof val === type });
+class BaseValidator extends Validator<unknown> {
+	constructor() {
+		super("value", null);
 	}
 
-	protected condition(val: T): val is T {
-		return true;
+	assert(val: unknown): asserts val is unknown {}
+}
+
+class TypeSchema<T> extends SchemaCondition<unknown, T> {
+	readonly name: string;
+
+	constructor(type: string | null, name = "value") {
+		super(new BaseValidator(), `Expected type ${type}`, type, name);
+		this.name = name;
+	}
+
+	protected condition(val: unknown): val is T {
+		return typeof val === this.type;
 	}
 }
 
 class StringSchema extends TypeSchema<string> {
-	constructor() {
-		super("string");
+	constructor(name = "string") {
+		super("string", name);
 	}
 }
 
 class NumberSchema extends TypeSchema<number> {
-	constructor() {
-		super("number");
+	constructor(name = "number") {
+		super("number", name);
 	}
 }
 
 class UnionSchema<T> extends TypeSchema<T> {
 	private readonly options: Validator<T>[];
 
-	constructor(options: Validator<T>[]) {
-		super(null);
+	constructor(options: Validator<T>[], name = "union") {
+		super(null, name);
 		this.options = options;
 	}
 
@@ -114,15 +159,40 @@ type ObjectFieldValidators<T> = { [K in keyof T]: Validator<T[K]> };
 class ObjectSchema<T extends Record<string, unknown>> extends TypeSchema<T> {
 	private readonly fieldValidators: ObjectFieldValidators<T>;
 
-	constructor(fields: ObjectFieldValidators<T>) {
-		super("object");
+	constructor(fields: ObjectFieldValidators<T>, name?: string) {
+		super("object", name);
+		for (const key in fields) {
+			fields[key].name = `${this.name}.${key}`;
+		}
 		this.fieldValidators = fields;
 	}
 
-	protected condition(val: T): val is T {
+	protected condition(val: unknown): val is T {
+		if (typeof val != "object") return false;
 		if (val == null) return false;
 		for (let key in this.fieldValidators) {
-			if (!this.fieldValidators[key].check(val[key])) return false;
+			const fieldValidator = this.fieldValidators[key] as Validator<T[string]>;
+			fieldValidator.assert.call(
+				fieldValidator,
+				(val as Record<string, unknown>)[key]
+			);
+		}
+		return true;
+	}
+}
+
+class ArraySchema<T extends unknown[]> extends TypeSchema<T> {
+	private readonly valueValidator: Validator<T[number]>;
+
+	constructor(values: Validator<T[number]>, name?: string) {
+		super("array", name ?? `${values.name}[]`);
+		this.valueValidator = values;
+	}
+
+	protected condition(val: unknown): val is T {
+		if (!Array.isArray(val)) return false;
+		for (const item in val) {
+			this.valueValidator.assert(item);
 		}
 		return true;
 	}
@@ -135,30 +205,35 @@ class UndefinedSchema extends TypeSchema<undefined> {
 }
 
 const schema = {
-	string(): StringSchema {
-		return new StringSchema();
+	string(name?: string): StringSchema {
+		return new StringSchema(name);
 	},
 
-	number(): NumberSchema {
-		return new NumberSchema();
+	number(name?: string): NumberSchema {
+		return new NumberSchema(name);
 	},
 
 	object<T extends Record<string, unknown>>(
-		fields: ObjectFieldValidators<T>
+		fields: ObjectFieldValidators<T>,
+		name?: string
 	): ObjectSchema<T> {
-		return new ObjectSchema(fields);
+		return new ObjectSchema(fields, name);
 	},
 
 	undefined(): UndefinedSchema {
 		return new UndefinedSchema();
 	},
 
-	union<T>(...options: Validator<T>[]) {
-		return new UnionSchema(options);
+	union<T>(options: Validator<T>[], name?: string) {
+		return new UnionSchema(options, name);
 	},
 
 	optional<T>(validator: Validator<T>): UnionSchema<T | undefined> {
-		return schema.union(schema.undefined(), validator);
+		return schema.union([schema.undefined(), validator]);
+	},
+
+	array<T extends unknown[]>(values: Validator<T[number]>, name?: string) {
+		return new ArraySchema(values, name);
 	}
 };
 
