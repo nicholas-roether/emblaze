a4ae81f50facaf4439cd8d206443b2cdf7b1b650
Nicholas <nicholas.roether@t-online.de>
2022-10-19 17:57:21 +0200

    Remove unnecessary schema in models

--- src/models.ts
+++ src/models.ts
@@ -1,5 +1,3 @@
-import schema, { Validator } from "./utils/schema";
-
 interface User {
 	id: string;
 	name: string;
@@ -13,24 +11,5 @@ interface User {
 		awarder: number;
 	};
 }
-
-const userSchema: Validator<User> = schema.object(
-	{
-		id: schema.string(),
-		name: schema.string(),
-		displayName: schema.string(),
-		profilePicture: schema.string(),
-		created: schema.number().integer(),
-		karma: schema.object({
-			post: schema.number().integer(),
-			comment: schema.number().integer(),
-			awardee: schema.number().integer(),
-			awarder: schema.number().integer()
-		})
-	},
-	"user"
-);
-
-export { userSchema };
 
 export type { User };
