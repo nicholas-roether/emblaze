9e846dd7d3f7a436a36fcc6ea6b7ee0a620c5d57
Nicholas <nicholas.roether@t-online.de>
2022-10-21 00:54:57 +0200

    Fix reddit test endpoint

--- pages/api/reddit/test/[...href].ts
+++ pages/api/reddit/test/[...href].ts
@@ -4,7 +4,7 @@ import { Api, ApiError } from "../../../../src/utils/api";
 const TestReq = Api.handler(async (ctx) => {
 	const { href } = ctx.req.query;
 	if (!href) throw new ApiError(400, "No api href provided");
-	const url = typeof href == "string" ? href : href.join("/");
+	const url = "/" + (typeof href == "string" ? href : href.join("/"));
 	const data = await Reddit.test(ctx.session, url);
 	return data;
 });
