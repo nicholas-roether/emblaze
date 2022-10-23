b7ef108775d30395090d549a80f47b243ea35c31
Nicholas <nicholas.roether@t-online.de>
2022-10-23 17:17:04 +0200

    Fix url construction in reddit api test req

--- pages/api/reddit/test/[...href].ts
+++ pages/api/reddit/test/[...href].ts
@@ -3,11 +3,11 @@ import Reddit from "../../../../src/services/reddit";
 import { Api, ApiError } from "../../../../src/utils/api";
 
 const TestReq = Api.handler(async (ctx) => {
-	const { href } = ctx.req.query;
+	const { href, ...params } = ctx.req.query;
 	if (!href) throw new ApiError(400, "No api href provided");
 	const url = "/" + (typeof href == "string" ? href : href.join("/"));
 	try {
-		const data = await Reddit.test(ctx.session, url, ctx.req.query);
+		const data = await Reddit.test(ctx.session, url, params);
 		return data;
 	} catch (err) {
 		if (err instanceof AxiosError) {
