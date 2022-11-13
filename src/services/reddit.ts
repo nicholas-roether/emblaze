9b057ada03124d68f69fc7ff71e8b46b37322f51
Nicholas Roether <nicholas.roether@t-online.de>
2022-11-13 16:05:23 +0100

    Fix reddit api requests

--- src/services/reddit.ts
+++ src/services/reddit.ts
@@ -4,37 +4,31 @@ import schema, { Validator } from "~/utils/schema";
 import RedditAuth from "./reddit_auth";
 
 interface RedditAPIUser {
-	kind: "t2";
-	data: {
-		id: string;
-		name: string;
-		icon_img: string;
-		created_utc: number;
-		total_karma: number;
-		link_karma: number;
-		comment_karma: number;
-		awarder_karma: number;
-		awardee_karma: number;
-	};
+	id: string;
+	name: string;
+	icon_img: string;
+	created_utc: number;
+	total_karma: number;
+	link_karma: number;
+	comment_karma: number;
+	awarder_karma: number;
+	awardee_karma: number;
 }
 
 const redditAPIUserValidator: Validator<RedditAPIUser> = schema.object(
 	{
-		kind: schema.string().exact("t2"),
-		data: schema.object({
-			id: schema.string(),
-			name: schema.string(),
-			icon_img: schema.string(),
-			created_utc: schema.number().integer(),
-			total_karma: schema.number().integer(),
-			link_karma: schema.number().integer(),
-			comment_karma: schema.number().integer(),
-			awarder_karma: schema.number().integer(),
-			awardee_karma: schema.number().integer(),
-			subreddit: schema.object({
-				title: schema.string(),
-				public_description: schema.string()
-			})
+		id: schema.string(),
+		name: schema.string(),
+		icon_img: schema.string(),
+		created_utc: schema.number().integer(),
+		total_karma: schema.number().integer(),
+		link_karma: schema.number().integer(),
+		comment_karma: schema.number().integer(),
+		awarder_karma: schema.number().integer(),
+		awardee_karma: schema.number().integer(),
+		subreddit: schema.object({
+			title: schema.string(),
+			public_description: schema.string()
 		})
 	},
 	"RedditApiUser"
@@ -47,27 +41,34 @@ class Reddit {
 
 	public static async me(login: string): Promise<User> {
 		const res = await axios.get(
-			Reddit.ENDPOINT + "/me",
+			this.getRequestURL("/me"),
 			RedditAuth.createAuthConfig(login)
 		);
+		console.log(JSON.stringify(res.data));
 		return this.createUser(res.data);
 	}
 
 	private static createUser(body: unknown): User {
 		redditAPIUserValidator.assert(body);
 		return {
-			id: `t2_${body.data.id}`,
-			name: body.data.name,
-			profilePicture: body.data.icon_img,
-			created: body.data.created_utc,
+			id: `t2_${body.id}`,
+			name: body.name,
+			profilePicture: body.icon_img,
+			created: body.created_utc,
 			karma: {
-				total: body.data.total_karma,
-				post: body.data.link_karma,
-				comment: body.data.comment_karma,
-				awardee: body.data.awardee_karma,
-				awarder: body.data.awarder_karma
+				total: body.total_karma,
+				post: body.link_karma,
+				comment: body.comment_karma,
+				awardee: body.awardee_karma,
+				awarder: body.awarder_karma
 			}
 		};
+	}
+
+	private static getRequestURL(href: string): string {
+		const url = new URL(this.ENDPOINT + href);
+		url.searchParams.set("raw_json", "1");
+		return url.toString();
 	}
 
 	private static getEndpoint(): string {
