d1251b17beee9ee5bdbe1fe9002e05f78875f1a1
Nicholas Roether <nicholas.roether@t-online.de>
2022-11-19 19:20:55 +0100

    Redirect to /welcome when not logged in

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import Heading from "~/components/basic/heading";
 import Page from "~/components/page";
 import { useRouteData } from "solid-start";
 import UserContext from "~/components/context/user_context";
-import { createServerData$ } from "solid-start/server";
+import { createServerData$, redirect } from "solid-start/server";
 import Login from "~/services/login";
 import Reddit from "~/services/reddit";
 import { userValidator } from "~/models";
@@ -22,7 +22,7 @@ export default function Home() {
 export function routeData() {
 	return createServerData$(async (_, { request }) => {
 		const login = await Login.from(request);
-		if (!login) return undefined;
+		if (!login) throw redirect("/welcome");
 		const me = await Reddit.me(login);
 		userValidator.assert(me);
 		return me;
